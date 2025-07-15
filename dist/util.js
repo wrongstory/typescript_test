a9d4a4113420435c0818dc68b64db00315de127d
L <lmwoo0327@naver.com>
2025-07-15 17:07:18 +0900

    init: 프로젝트 생성

---
+++ dist/util.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.printName = void 0;
+const printName = (name) => {
+    console.log(name);
+};
+exports.printName = printName;
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// console.log("Hellow TypeScript");
+
+// const nums: number[] = [];
+
+// const a = 10 + nums[0];
+
+// const someString = "typescript!";
+
+// someString.endsWith("!");
+
+import { printName } from "./util";
+
+printName("typescript");
