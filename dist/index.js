35bf2d2fb84de6e9d5da142f51ec5341fb136ab2
L <lmwoo0327@naver.com>
2025-07-15 17:10:57 +0900

    Remove node_modules and dist from repository

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// console.log("Hellow TypeScript");
-Object.defineProperty(exports, "__esModule", { value: true });
-// const nums: number[] = [];
-// const a = 10 + nums[0];
-// const someString = "typescript!";
-// someString.endsWith("!");
-const util_1 = require("./util");
-(0, util_1.printName)("typescript");
--- dist/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.printName = void 0;
-const printName = (name) => {
-    console.log(name);
-};
-exports.printName = printName;
